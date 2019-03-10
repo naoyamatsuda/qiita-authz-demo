f4df4eebe017ad5679c2ddb464b2991271ddeb64
matsuda_naoya <>
2019-03-10 16:42:00 +0900

    [add]:authzルーティングを追加

---
+++ authz/authz.js
@@ -0,0 +1,5 @@
+const authz = (req, res) => {
+  console.log('test')
+}
+
+module.exports = authz
--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express')
 
+const authz = require('./authz/authz')
+
 // Constants
 const PORT = 3000
 
 // App
 const app = express()
+app.get('/authz', authz)
 
-app.get('/', (req, res) => {
-  res.send('Hello Node.js Sample!\n')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello Node.js Sample!\n')
+// })
 
 const port = process.env.PORT || PORT
 app.listen(port)
