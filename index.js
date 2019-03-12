540c4d0c9798bd3f5a21e71d43faeef5fd86d516
matsuda_naoya <>
2019-03-12 20:04:58 +0900

    [add]:get redirectionエンドポイントの追加

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 
 const authz = require('./authz/authz')
+const redirection = require('./authz/redirection')
 
 // Constants
 const PORT = 3000
@@ -8,6 +9,7 @@ const PORT = 3000
 // App
 const app = express()
 app.get('/authz', authz)
+app.get('/authz/redirection', redirection)
 
 const port = process.env.PORT || PORT
 app.listen(port)
